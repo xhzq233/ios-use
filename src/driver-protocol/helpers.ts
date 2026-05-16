5e6dbd06afe20c1147dba0707b5bae0c3316e810
xiahouzhen <xiahouzhen.1@bytedance.com>
2026-05-16 20:07:14 +0800

    Remove legacy TypeScript host

--- src/driver-protocol/helpers.ts
+++
@@ -1,7 +0,0 @@
-export function omitUndefined<T extends Record<string, unknown>>(obj: T): Record<string, unknown> {
-  const out: Record<string, unknown> = {};
-  for (const [key, value] of Object.entries(obj)) {
-    if (value !== undefined) out[key] = value;
-  }
-  return out;
-}
