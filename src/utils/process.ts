d1db9a0e599e970a4252b73cc4e026a89595c7f2
xiahouzhen <xhzq233@qq.com>
2026-05-16 20:07:14 +0800

    Remove legacy TypeScript host

--- src/utils/process.ts
+++
@@ -1,9 +0,0 @@
-export function isProcessAlive(pid: number | undefined): boolean {
-  if (!pid) return false;
-  try {
-    process.kill(pid, 0);
-    return true;
-  } catch {
-    return false;
-  }
-}
