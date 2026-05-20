db78c16a702ee2173aa0c715461abf9cc7efc720
xiahouzhen <xiahouzhen.1@bytedance.com>
2026-05-20 13:14:05 +0800

    chore: bump version to 1.0.2

--- swift-cli/Tests/IOSUseCLITests/IOSUseCLITests.swift
+++ swift-cli/Tests/IOSUseCLITests/IOSUseCLITests.swift
@@ -4,7 +4,7 @@ import IOSUseProtocol
 
 final class IOSUseCLITests: XCTestCase {
     func testVersionMatchesCurrentPackageVersion() {
-        XCTAssertEqual(IOSUseCLI.version, "1.0.1")
+        XCTAssertEqual(IOSUseCLI.version, "1.0.2")
     }
 
     func testDaemonRunnerRequiresACommand() throws {
