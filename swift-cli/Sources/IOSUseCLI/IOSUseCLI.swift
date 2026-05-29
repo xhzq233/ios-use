7775e3876671b070db76a9322278d15e51131912
xiahouzhen <xhzq233@qq.com>
2026-05-29 13:09:01 +0800

    Fix nslog dual-stack listener

--- swift-cli/Sources/IOSUseCLI/IOSUseCLI.swift
+++ swift-cli/Sources/IOSUseCLI/IOSUseCLI.swift
@@ -4,7 +4,7 @@ import IOSUseProtocol
 public struct IOSUseCLI: Sendable {
     public typealias CLIOutputSink = @Sendable (String) -> Void
 
-    public static let version = "1.1.1"
+    public static let version = "1.1.2"
     static var driverClientFactoryForTesting: ((SessionService.Info) -> DriverCommandClient)? {
         get { DriverCommandExecution.clientFactoryForTesting }
         set { DriverCommandExecution.clientFactoryForTesting = newValue }
