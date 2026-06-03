fcded02930390a13c43fb93d912b4a139c1f7bd7
xiahouzhen <xhzq233@qq.com>
2026-06-04 01:16:15 +0800

    fix: restore real-device driver startup

--- driver/ui/DriverLog.swift
+++ driver/ui/DriverLog.swift
@@ -1,11 +1,40 @@
-import os.log
+import Foundation
 
 enum DriverLog {
+    typealias Sink = (String) -> Void
+
+    private static let sinkLock = NSLock()
+    private static var sink: Sink = { message in
+        NSLog("%@", message)
+    }
+
     static func info(_ message: String) {
-        os_log("%{public}@", log: .default, type: .info, message)
+        write(message)
     }
 
     static func error(_ message: String) {
-        os_log("%{public}@", log: .default, type: .error, message)
+        write(message)
     }
+
+    private static func write(_ message: String) {
+        sinkLock.lock()
+        let currentSink = sink
+        sinkLock.unlock()
+        currentSink(message)
+    }
+
+    #if DEBUG
+    static func withSinkForTesting<T>(_ replacement: @escaping Sink, body: () throws -> T) rethrows -> T {
+        sinkLock.lock()
+        let previousSink = sink
+        sink = replacement
+        sinkLock.unlock()
+        defer {
+            sinkLock.lock()
+            sink = previousSink
+            sinkLock.unlock()
+        }
+        return try body()
+    }
+    #endif
 }
