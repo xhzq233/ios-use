36dca621e64f2ca46eb71a9f411a0f80c58bb4d5
xiahouzhen <xhzq233@qq.com>
2026-06-04 09:54:19 +0800

    Revert "fix: restore real-device driver startup"
    This reverts commit fcded02930390a13c43fb93d912b4a139c1f7bd7.

--- driver/runner/DriverAutostart.m
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "IOSUseDriver-Swift.h"
-
-__attribute__((constructor))
-static void IOSUseDriverAutostart(void) {
-    NSLog(@"%@", @"[debug][xctest-autostart] IOSUseDriver bundle loaded");
-    [DriverServer startSharedIfNeeded];
-}
